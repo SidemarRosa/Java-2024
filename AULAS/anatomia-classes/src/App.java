fabd2635569d573d346397e182b4f377808ae1e9
SidemarRosa <sidr.com.slr@gmail.com>
2024-08-12 22:44:31 -0300

    aula 2
    Classes e variáveis

--- AULAS/anatomia-classes/src/App.java
+++ AULAS/anatomia-classes/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("TO MALUCO PROGRAMANDO EM JAVA CARAI");
+        String meuNome = "Sidemar";
+        String meuSobrenome = "Rosa";
+        int anoNascimento = 1998;
+        int idade = 2024 - anoNascimento;
+
+         System.out.println( "Olá Me chamo" + meuNome + "" + meuSobrenome + " Nasci em " + anoNascimento + " Idade " + idade);
     }
 }
